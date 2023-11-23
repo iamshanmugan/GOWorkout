48fe6507728d18e59d1ffa38c54c88a84210ec2c
iamshanmugan <75529059+iamshanmugan@users.noreply.github.com>
2023-11-23 11:53:01 -0500

    Update pointerBasic.go

--- Pointer/pointerBasic.go
+++ Pointer/pointerBasic.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Go pointer learning....")
-
+	
 	var name string
 	name = "shaan"
 	fmt.Println(name)
-
+	//address of the variable
 	ptr := &name
 	fmt.Println(ptr)
 
