7bfd9c9f19fe08e60e9217f3c5fd17117779a016
iamshanmugan <iamshanmugan@gmail.com>
2023-11-24 11:23:19 -0500

    timoutsamp

---
+++ Channel/Timeout.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c1 := make(chan string, 1)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		c1 <- "channel 1 message"
+	}()
+
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	case <-time.After(1 * time.Second):
+		fmt.Println("timeout")
+
+	}
+
+	c2 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "channel 2 message"
+	}()
+
+	select {
+	case res := <-c2:
+		fmt.Println(res)
+	case <-time.After(3 * time.Second):
+		fmt.Println("timeout2")
+	}
+}
