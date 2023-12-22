7787b8a556e220bd5241954fea5d6154f177cfa2
iamshanmugan <iamshanmugan@gmail.com>
2023-12-22 11:46:22 -0500

    forloop update

---
+++ Loops/breakContsamp.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+
+}
